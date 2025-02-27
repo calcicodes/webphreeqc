a3bef45105e88f318eef12b2976b15944fb456af
Oscar Branson <ob266@cam.ac.uk>
2025-02-27 00:57:24 +0000

    improved start text

--- webphreeqc/src/utils/phreeqc_inputs.py
+++ webphreeqc/src/utils/phreeqc_inputs.py
@@ -3,9 +3,15 @@ from pathlib import Path
 template_path = Path(__file__).parent / 'input_templates'
 
 PHREEQC_INPUT_TEMPLATES = {
-    "Blank": ""
+    "Blank": """# Write your PHREEQC input file here, or select a template from the dropdown above. 
+    
+# See the PHREEQC documentation for details: https://water.usgs.gov/water-resources/software/PHREEQC/documentation/phreeqc3-html/phreeqc3.htm
+
+# The EXAMPLES pages are particularly useful: https://water.usgs.gov/water-resources/software/PHREEQC/documentation/phreeqc3-html/phreeqc3-62.htm
+"""
 }
 
-for template in template_path.glob('*.phreeqc'):
+for template in sorted(template_path.glob('*.phreeqc')):
     with open(template, 'r') as f:
         PHREEQC_INPUT_TEMPLATES[template.stem] = f.read()
+
